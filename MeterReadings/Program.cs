b6e64f353e98e4aa5ad7815b3bbfe7d16d22fe5f
Hywel Rees <hjr555@gmail.com>
2017-01-06 14:41:15 +0000

    Initial working code

---
+++ MeterReadings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication8
+{
+    public class Reading
+    {
+        public DateTime Date { get; set; }
+        public int Day { get; set; }
+        public int Night { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var datapath = @"Data\readings.json";
+
+            var engineers = new Dictionary<string, int>();
+            IEnumerable<Reading> data;
+
+            using (var r = new StreamReader(datapath))
+            {
+                var json = r.ReadToEnd();
+
+                data = JsonConvert.DeserializeObject<List<Reading>>(json)
+                    .OrderByDescending(x => x.Date);
+            }
+
+            Console.WriteLine($"Total readings: { data.Count() }");
+            Console.WriteLine();
+
+            foreach(var reading in data)
+            {
+                Console.WriteLine($"Date: { reading.Date.ToShortDateString() }\t Day: { reading.Day }\t Night: { reading.Night }");
+            }
+        }
+    }
+}
